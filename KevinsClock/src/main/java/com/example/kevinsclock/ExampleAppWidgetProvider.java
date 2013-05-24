9fc950419cf08795767a9d10d1c8a85fbbf956f6
KevinKoleckar <Kevin.Koleckar@positScience.com>
2013-05-24 15:37:12 -0700

    second attempt at commits

---
+++ KevinsClock/src/main/java/com/example/kevinsclock/ExampleAppWidgetProvider.java
@@ -0,0 +1,34 @@
+package com.example.kevinsclock;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetProvider;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+/**
+ * Created by test on 5/24/13.
+ */
+public class ExampleAppWidgetProvider {
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, FullscreenActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_appwidget);
+            views.setOnClickPendingIntent(R.id.button, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
